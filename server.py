689b96628b4736d88abc0461910f5dc12239bb6a
Yahir Leo <yahirrocki@outlook.com>
2025-01-18 22:47:46 -0600

    added ping status from host

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 import grpc
 from concurrent import futures
+
 from generated import sendToken_pb2
 from generated import sendToken_pb2_grpc
+
+# from generated import status_pb2
+from generated import status_pb2_grpc
 
 
 class VerifyTokenService(sendToken_pb2_grpc.VerifyTokenServiceServicer):
@@ -10,8 +14,13 @@ class VerifyTokenService(sendToken_pb2_grpc.VerifyTokenServiceServicer):
         Simple verification example - needed db validation token
         """
         if request.token == "my_test_token":
-            return sendToken_pb2.VerifyTokenResponse(user="Edgardo", success=True)
+            return sendToken_pb2.VerifyTokenResponse(user="Yiro", success=True)
         return sendToken_pb2.VerifyTokenResponse(success=False)
+
+
+class StatusService(status_pb2_grpc.Status):
+    def verifyStatus(self, request, context):
+        print(request.user)
 
 
 def serve():
@@ -19,6 +28,8 @@ def serve():
     sendToken_pb2_grpc.add_VerifyTokenServiceServicer_to_server(
         VerifyTokenService(), server
     )
+    status_pb2_grpc.add_StatusServicer_to_server(StatusService(), server)
+
     server.add_insecure_port("[::]:50051")
     print("gRPC server listening on port 50051...")
     server.start()
